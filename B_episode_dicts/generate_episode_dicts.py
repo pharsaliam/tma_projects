c411913a1789205b3667b38f7d4c4542ff636488
pharsaliam <jenniferz7633@gmail.com>
2022-05-28 10:35:27 -0400

    Move arg parse to main

--- B_episode_dicts/generate_episode_dicts.py
+++ B_episode_dicts/generate_episode_dicts.py
@@ -9,48 +9,18 @@ sys.path.insert(1, p)
 from utils import create_logger, save_dict_as_pkl
 from B_episode_dicts.tma_episode_processor import TMAEpisode
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    '--start_episode', '-S',
-    type=int,
-    default=1,
-    choices=range(1, 161),
-    help='First episode to include in the dict'
-)
-parser.add_argument(
-    '--end_episode', '-E',
-    type=int,
-    default=160,
-    choices=range(1, 161),
-    help='Last episode to include in the dict'
-)
-parser.add_argument(
-    '--logging_level', '-L',
-    type=str.upper,
-    default='info',
-    help='Python logging level'
-)
-parser.add_argument(
-    '--save_dir', '-D',
-    type=str,
-    default='B_episode_dicts/dicts',
-    help='Directory to which to save the episode dicts'
-)
-args = parser.parse_args()
-if args.end_episode < args.start_episode:
-    parser.error('Start episode # must be less than end episode #')
-logger = create_logger('episode_dicts', logging_level=args.logging_level.upper())
 
-
-def generate_individual_episode_dict(start_episode, end_episode):
+def generate_individual_episode_dict(start_episode, end_episode, logger_object=None):
     list_of_episodes = [i for i in range(start_episode, end_episode+1)]
-    logger.debug(f'{list_of_episodes=}')
+    if logger_object:
+        logger_object.debug(f'{list_of_episodes=}')
     individual_episode_dict = {}
     edge_appearance_dict = {}
     node_appearance_dict = {}
     for e in list_of_episodes:
-        episode = TMAEpisode(e, logging_level=args.logging_level)
-        logger.info(f'Episode {e} created')
+        episode = TMAEpisode(e)
+        if logger_object:
+            logger_object.info(f'Episode {e} created')
         episode()
         individual_episode_dict[e] = {
             'nodes_dict': episode.nodes_dict,
@@ -77,7 +47,7 @@ def update_item_appearance_dict(items_dict, item_appearance_dict, episode_number
             item_appearance_dict[item] = {episode_number: item_attributes}
 
 
-def generate_cumulative_episode_dict(individual_episode_dict):
+def generate_cumulative_episode_dict(individual_episode_dict, logger_object=None):
     cumulative_episode_dict = {}
     for e in individual_episode_dict:
         prev_e = e - 1
@@ -98,8 +68,9 @@ def generate_cumulative_episode_dict(individual_episode_dict):
             }
         else:
             cumulative_episode_dict[e] = copy.deepcopy(individual_episode_dict[e])
-        logger.info(f'Generated cumulative episode dict for episode {e}')
-        logger.debug(f'{cumulative_episode_dict}')
+        if logger_object:
+            logger_object.info(f'Generated cumulative episode dict for episode {e}')
+            logger_object.debug(f'{cumulative_episode_dict}')
     return cumulative_episode_dict
 
 
@@ -118,11 +89,43 @@ def update_cumulative_items_dict(item_type, previous_episode_dict, current_episo
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--start_episode', '-S',
+        type=int,
+        default=1,
+        choices=range(1, 161),
+        help='First episode to include in the dict'
+    )
+    parser.add_argument(
+        '--end_episode', '-E',
+        type=int,
+        default=160,
+        choices=range(1, 161),
+        help='Last episode to include in the dict'
+    )
+    parser.add_argument(
+        '--logging_level', '-L',
+        type=str.upper,
+        default='info',
+        help='Python logging level'
+    )
+    parser.add_argument(
+        '--save_dir', '-D',
+        type=str,
+        default='B_episode_dicts/dicts',
+        help='Directory to which to save the episode dicts'
+    )
+    args = parser.parse_args()
+    if args.end_episode < args.start_episode:
+        parser.error('Start episode # must be less than end episode #')
+    logger = create_logger('episode_dicts',
+                           logging_level=args.logging_level.upper())
     logger.info(vars(args))
-    indi, ea, na = generate_individual_episode_dict(args.start_episode, args.end_episode)
+    indi, ea, na = generate_individual_episode_dict(args.start_episode, args.end_episode, logger)
     logger.info('Finished generating individual episode dict, node appearance dict, and edge appearance dict')
     logger.debug(f'Ending episode (i): {indi[args.end_episode]}')
-    cumu = generate_cumulative_episode_dict(indi)
+    cumu = generate_cumulative_episode_dict(indi, logger)
     logger.info('Finished generating cumulative episode dict')
     logger.debug(f'Ending episode (c): {cumu[args.end_episode]}')
     save_dict_as_pkl(na, 'na', args.save_dir, logger)
