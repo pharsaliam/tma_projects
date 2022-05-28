168417cbff1953e619716ea4509d612b07114a40
pharsaliam <jenniferz7633@gmail.com>
2022-05-27 23:04:48 -0400

    Update docstrings

--- A_episode_texts/extract_episode_text_from_epub.py
+++ A_episode_texts/extract_episode_text_from_epub.py
@@ -1,3 +1,12 @@
+"""
+This script parses an ebook of The Magnus Archives Transcripts to separate
+episode texts by episode.
+
+It assumes that an .epub files containing all the transcripts is saved as
+A_episode_texts/texts/the_magnus_archives.epub.
+
+It generates and saves the file A_episode_texts/texts/tma_text_from_epub.pkl.
+"""
 import re
 import pickle
 
